4110813f70d57ad5305fa83c039884d2f4cf7b5f
Satyadeep2023 <125433274+Satyadeep2023@users.noreply.github.com>
2023-03-16 12:52:24 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -2,7 +2,7 @@ public class Add {
   public static void main(String[] args) {
     // two integer variables with values
     // and a variable "sum" to store the result
-    int num1 = 10, num2 = 15,sum;
+    int num1 = 10, num2 = 100,sum;
 
     //calculating the sum of num1 and num2 and
     //storing the result in the variable sum
