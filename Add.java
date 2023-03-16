9bb2585dc9eda6520625943a591be4389c4b978a
Satyadeep Bhardwaj <satyadeep.bhardwaj@kyndryl.com>
2023-03-16 13:45:57 +0530

    Updated Values

--- Add.java
+++ Add.java
@@ -2,7 +2,7 @@ public class Add {
   public static void main(String[] args) {
     // two integer variables with values
     // and a variable "sum" to store the result
-    int num1 = 10, num2 = 15,sum;
+    int num1 = 120, num2 = 120,sum;
 
     //calculating the sum of num1 and num2 and
     //storing the result in the variable sum
